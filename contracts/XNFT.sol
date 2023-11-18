d59d9682025022e2e9b155733ce2353a9cfcf4ba
skyh24 <skyhighfeng@gmail.com>
2023-11-18 11:31:50 +0800

    first commit

---
+++ contracts/XNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721.sol";
+
+contract XNFT is ERC721{
+    uint private _nextTokenId;
+
+    // 构造函数
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_){
+    }
+
+    //BAYC的baseURI为ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/ 
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    }
+    
+    // 铸造函数
+    function mint(address to, uint tokenId) external {
+        uint tokenId = _nextTokenId++;
+        _mint(to, tokenId);
+    }
+}
